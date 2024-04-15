363e7803a91e0742fc663cc6d563e281cbd14704
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-04-15 17:48:12 +0600

    main jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { router } from "./routes/Routes.jsx"
+import { RouterProvider } from "react-router-dom"
+import { HelmetProvider } from "react-helmet-async"
+import FirebaseProvider from "./firebaseProvider/FirebaseProvider.jsx"
+import { Toaster } from "react-hot-toast"
+import "aos/dist/aos.css"
+import AOS from "aos"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+AOS.init({ duration: 1000 })
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<FirebaseProvider>
+		<HelmetProvider>
+			<RouterProvider router={router} />
+			<Toaster />
+		</HelmetProvider>
+	</FirebaseProvider>
 )
